26bee46d74ed0454106128aba527803d59dfffbc
bhartendu1112 <attribhartendu@gmail.com>
2022-01-29 12:13:37 +0530

    can_drive submission

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,4 +1,13 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
+    if(hasDrivingLiscence === false){
+        console.log("You cannot drive")
+
+    }else if (isTired === true || isSober === false){
+        console.log(" You shouldn't drive ")
+
+    }else {
+        console.log("You can drive")
+    }
     // Write you code here
 }
 
